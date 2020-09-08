fef3572147615a12d039f745c3307297cd3e3eb4
zhuc-boy <zhuc1992@icloud.com>
2020-09-08 15:25:31 +0800

    数组计算

--- lib/matrix4.ts
+++ lib/matrix4.ts
@@ -147,7 +147,16 @@ namespace matrix {
 
             return this;
         }
-        // 正射投影矩阵
+        /**
+        *正射投影矩阵
+        * @param left 剪切面左侧坐标
+        * @param right 剪切面右侧坐标
+        * @param bottom 剪切面底部坐标
+        * @param top 剪切面顶部坐标
+        * @param top 剪切面顶部坐标
+        * @param near 剪切面近处深度坐标
+        * @param far 剪切面远处深度坐标
+        */
         setOrtho(left: number, right: number, bottom: number, top: number, near: number, far: number) {
             let e, rw: number, rh: number, rd: number;
             if (left === right || bottom === top || near === far) {
@@ -182,7 +191,16 @@ namespace matrix {
         ortho(left: number, right: number, bottom: number, top: number, near: number, far: number) {
             return this.concat(new Matrix4().setOrtho(left, right, bottom, top, near, far))
         }
-        // 圆锥投影矩阵
+        /**
+        *锥体投影矩阵
+        * @param left 剪切面左侧坐标
+        * @param right 剪切面右侧坐标
+        * @param bottom 剪切面底部坐标
+        * @param top 剪切面顶部坐标
+        * @param top 剪切面顶部坐标
+        * @param near 剪切面近处深度坐标
+        * @param far 剪切面远处深度坐标
+        */
         setFrustum(left: number, right: number, bottom: number, top: number, near: number, far: number) {
             let e, rw: number, rh: number, rd: number;
             if (left === right || top === bottom || near === far) {
@@ -226,17 +244,293 @@ namespace matrix {
         frustum(left: number, right: number, bottom: number, top: number, near: number, far: number) {
             return this.concat(new Matrix4().setFrustum(left, right, bottom, top, near, far))
         }
+
+        /**
+         * 视角投影
+         * @param {number} fovy The angle between the upper and lower sides of the frustum.
+         * @param aspect The aspect ratio of the frustum.(width / height)
+         * @param near The distances to the nearer depth clipping plane.This value must be plus value.
+         * @param far The distances to the farther depth clipping plane.This value must be plus value.
+         */
+        setPerspective(fovy: number, aspect: number, near: number, far: number) {
+            let e, rd, s, ct;
+            if (near === far || aspect === 0) {
+                throw "null frustum"
+            }
+            if (near <= 0) {
+                throw "near<=0"
+            }
+            if (far <= 0) {
+                throw "far<=0"
+            }
+            fovy = Math.PI * fovy / 180 / 2;
+            s = Math.sin(fovy)
+            if (s === 0) {
+                throw "null frustum"
+            }
+            rd = 1 / (far - near);
+            ct = Math.cos(fovy) / s;
+            e = this.elements
+            e[0] = ct / aspect;
+            e[1] = 0;
+            e[2] = 0;
+            e[3] = 0;
+
+            e[4] = 0;
+            e[5] = ct;
+            e[6] = 0;
+            e[7] = 0;
+
+            e[8] = 0;
+            e[9] = 0;
+            e[10] = -(far + near) * rd;
+            e[11] = -1;
+
+            e[12] = 0;
+            e[13] = 0;
+            e[14] = -2 * near * far * rd;
+            e[15] = 0;
+
+            return this;
+        }
+
+        perspective(fovy: number, aspect: number, near: number, far: number) {
+            return this.concat(new Matrix4().setPerspective(fovy, aspect, near, far))
+        }
+        /**
+         * 设置缩放矩阵
+         * @param  {number} x X轴坐标
+         * @param  {number} y Y轴坐标
+         * @param  {number} z Z轴坐标
+         */
+        setScale(x: number, y: number, z: number) {
+            let e = this.elements
+            e[0] = x; e[4] = 0; e[8] = 0; e[12] = 0;
+            e[1] = 0; e[5] = y; e[9] = 0; e[13] = 0;
+            e[2] = 0; e[6] = 0; e[10] = z; e[14] = 0;
+            e[3] = 0; e[7] = 0; e[11] = 0; e[15] = 1;
+            return this;
+        }
+        /**
+         * scale.
+         * @param  {number} x X轴坐标
+         * @param  {number} y X轴坐标
+         * @param  {number} z Z轴坐标
+         */
+        scale(x: number, y: number, z: number) {
+            var e = this.elements;
+            e[0] *= x; e[4] *= y; e[8] *= z;
+            e[1] *= x; e[5] *= y; e[9] *= z;
+            e[2] *= x; e[6] *= y; e[10] *= z;
+            e[3] *= x; e[7] *= y; e[11] *= z;
+            return this;
+        }
+        setTranslate(x: number, y: number, z: number) {
+            var e = this.elements;
+            e[0] = 1; e[4] = 0; e[8] = 0; e[12] = x;
+            e[1] = 0; e[5] = 1; e[9] = 0; e[13] = y;
+            e[2] = 0; e[6] = 0; e[10] = 1; e[14] = z;
+            e[3] = 0; e[7] = 0; e[11] = 0; e[15] = 1;
+            return this;
+        }
+        translate(x: number, y: number, z: number) {
+            var e = this.elements;
+            e[12] += e[0] * x + e[4] * y + e[8] * z;
+            e[13] += e[1] * x + e[5] * y + e[9] * z;
+            e[14] += e[2] * x + e[6] * y + e[10] * z;
+            e[15] += e[3] * x + e[7] * y + e[11] * z;
+            return this;
+        }
+        /**
+        * Set the matrix for rotation.
+        * The vector of rotation axis may not be normalized.
+        * @param angle The angle of rotation (degrees)
+        * @param x The X coordinate of vector of rotation axis.
+        * @param y The Y coordinate of vector of rotation axis.
+        * @param z The Z coordinate of vector of rotation axis.
+        * @return this
+        */
+        setRotate(angle: number, x: number, y: number, z: number) {
+            var e, s, c, len, rlen, nc, xy, yz, zx, xs, ys, zs;
+
+            angle = Math.PI * angle / 180;
+            e = this.elements;
+
+            s = Math.sin(angle);
+            c = Math.cos(angle);
+
+            if (0 !== x && 0 === y && 0 === z) {
+                // Rotation around X axis
+                if (x < 0) {
+                    s = -s;
+                }
+                e[0] = 1; e[4] = 0; e[8] = 0; e[12] = 0;
+                e[1] = 0; e[5] = c; e[9] = -s; e[13] = 0;
+                e[2] = 0; e[6] = s; e[10] = c; e[14] = 0;
+                e[3] = 0; e[7] = 0; e[11] = 0; e[15] = 1;
+            } else if (0 === x && 0 !== y && 0 === z) {
+                // Rotation around Y axis
+                if (y < 0) {
+                    s = -s;
+                }
+                e[0] = c; e[4] = 0; e[8] = s; e[12] = 0;
+                e[1] = 0; e[5] = 1; e[9] = 0; e[13] = 0;
+                e[2] = -s; e[6] = 0; e[10] = c; e[14] = 0;
+                e[3] = 0; e[7] = 0; e[11] = 0; e[15] = 1;
+            } else if (0 === x && 0 === y && 0 !== z) {
+                // Rotation around Z axis
+                if (z < 0) {
+                    s = -s;
+                }
+                e[0] = c; e[4] = -s; e[8] = 0; e[12] = 0;
+                e[1] = s; e[5] = c; e[9] = 0; e[13] = 0;
+                e[2] = 0; e[6] = 0; e[10] = 1; e[14] = 0;
+                e[3] = 0; e[7] = 0; e[11] = 0; e[15] = 1;
+            } else {
+                // Rotation around another axis
+                len = Math.sqrt(x * x + y * y + z * z);
+                if (len !== 1) {
+                    rlen = 1 / len;
+                    x *= rlen;
+                    y *= rlen;
+                    z *= rlen;
+                }
+                nc = 1 - c;
+                xy = x * y;
+                yz = y * z;
+                zx = z * x;
+                xs = x * s;
+                ys = y * s;
+                zs = z * s;
+
+                e[0] = x * x * nc + c;
+                e[1] = xy * nc + zs;
+                e[2] = zx * nc - ys;
+                e[3] = 0;
+
+                e[4] = xy * nc - zs;
+                e[5] = y * y * nc + c;
+                e[6] = yz * nc + xs;
+                e[7] = 0;
+
+                e[8] = zx * nc + ys;
+                e[9] = yz * nc - xs;
+                e[10] = z * z * nc + c;
+                e[11] = 0;
+
+                e[12] = 0;
+                e[13] = 0;
+                e[14] = 0;
+                e[15] = 1;
+            }
+
+            return this;
+        }
+        rotate(angle: number, x: number, y: number, z: number) {
+            return this.concat(new Matrix4().setRotate(angle, x, y, z));
+        }
+        /**
+         * Set the viewing matrix.
+         * @param  {number} eyeX
+         * @param  {number} eyeY
+         * @param  {number} eyeZ The position of the eye point.
+         * @param  {number} centerX
+         * @param  {number} centerY
+         * @param  {number} centerZ The position of the reference point.
+         * @param  {number} upX
+         * @param  {number} upY
+         * @param  {number} upZ The direction of the up vector.
+         */
+        setLookAt(eyeX: number, eyeY: number, eyeZ: number, centerX: number, centerY: number, centerZ: number, upX: number, upY: number, upZ: number) {
+            var e, fx, fy, fz, rlf, sx, sy, sz, rls, ux, uy, uz;
+
+            fx = centerX - eyeX;
+            fy = centerY - eyeY;
+            fz = centerZ - eyeZ;
+
+            // Normalize f.
+            rlf = 1 / Math.sqrt(fx * fx + fy * fy + fz * fz);
+            fx *= rlf;
+            fy *= rlf;
+            fz *= rlf;
+
+            // Calculate cross product of f and up.
+            sx = fy * upZ - fz * upY;
+            sy = fz * upX - fx * upZ;
+            sz = fx * upY - fy * upX;
+
+            // Normalize s.
+            rls = 1 / Math.sqrt(sx * sx + sy * sy + sz * sz);
+            sx *= rls;
+            sy *= rls;
+            sz *= rls;
+
+            // Calculate cross product of s and f.
+            ux = sy * fz - sz * fy;
+            uy = sz * fx - sx * fz;
+            uz = sx * fy - sy * fx;
+
+            // Set to this.
+            e = this.elements;
+            e[0] = sx;
+            e[1] = ux;
+            e[2] = -fx;
+            e[3] = 0;
+
+            e[4] = sy;
+            e[5] = uy;
+            e[6] = -fy;
+            e[7] = 0;
+
+            e[8] = sz;
+            e[9] = uz;
+            e[10] = -fz;
+            e[11] = 0;
+
+            e[12] = 0;
+            e[13] = 0;
+            e[14] = 0;
+            e[15] = 1;
+
+            // Translate.
+            return this.translate(-eyeX, -eyeY, -eyeZ);
+        }
+        lookAt(eyeX: number, eyeY: number, eyeZ: number, centerX: number, centerY: number, centerZ: number, upX: number, upY: number, upZ: number) {
+            return this.concat(new Matrix4().setLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ));
+        }
     }
     export class Vector3 {
-        elements: any
-        constructor() {
-
+        elements: Float32Array
+        constructor(opt_src?: Float32Array) {
+            let v = new Float32Array(3)
+            if (opt_src && typeof opt_src === "object") {
+                v[0] = opt_src[0]; v[1] = opt_src[1]; v[2] = opt_src[2];
+            }
+            this.elements = v
+        }
+        normalize() {
+            let v = this.elements
+            var c = v[0], d = v[1], e = v[2], g = Math.sqrt(c * c + d * d + e * e);
+            if (g) {
+                if (g == 1)
+                    return this;
+            } else {
+                v[0] = 0; v[1] = 0; v[2] = 0;
+                return this;
+            }
+            g = 1 / g;
+            v[0] = c * g; v[1] = d * g; v[2] = e * g;
+            return this;
         }
     }
     export class Vector4 {
-        elements: any
-        constructor() {
-
+        elements: Float32Array
+        constructor(opt_src?: Float32Array) {
+            let v = new Float32Array(4)
+            if (opt_src && typeof opt_src === "object") {
+                v[0] = opt_src[0]; v[1] = opt_src[1]; v[2] = opt_src[2]; v[3] = opt_src[3];
+            }
+            this.elements = v
         }
     }
 }
